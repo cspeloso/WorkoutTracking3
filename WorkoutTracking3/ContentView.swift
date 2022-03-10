8b1cc2a6fa800de850fc2e7c6d2dd7001038c888
cspeloso <chrispeloso2@gmail.com>
2022-03-09 23:25:33 -0500

    Initial Commit

---
+++ WorkoutTracking3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WorkoutTracking3
+//
+//  Created by Chris Peloso on 3/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
