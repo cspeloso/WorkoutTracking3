dd275331649a72fc5285fc32155bb9c96b7773b0
cspeloso <chrispeloso2@gmail.com>
2023-10-31 12:32:00 -0400

    Update 1.0.4

---
+++ WorkoutTracking3/Views/UserRoutines/MostRecentLoggedSetView.swift
@@ -0,0 +1,18 @@
+//
+//  MostRecentLoggedSetView.swift
+//  WorkoutTracking3
+//
+//  Created by Chris Peloso on 10/31/23.
+//
+
+import SwiftUI
+
+struct MostRecentLoggedSetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MostRecentLoggedSetView()
+}
