fbb1264efdaee00d84f94d4b2fd85b4285e29dbe
cspeloso <chrispeloso2@gmail.com>
2024-01-13 23:30:10 -0500

    Version 1.05, various minor bug fixes. Began work on an Apple Watch version of the app as well to accompany the main app.

--- Shared/Bundle-Decodable.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Bundle-Decode.swift
-//  WorkoutTracking3
-//
-//  Created by Chris Peloso on 1/12/24.
-//
-
-import Foundation
--- Shared/Structs/Exercise.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Exercise.swift
-//  Work It Out Apple Watch Watch App
-//
-//  Created by Chris Peloso on 1/12/24.
-//
-
-import Foundation
